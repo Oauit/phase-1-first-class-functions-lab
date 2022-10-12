22f1d025d0557c30350ef29c0723c3ace9e8c8b1
oauit <aidanjade.ward@gmail.com>
2022-10-12 15:12:10 -0600

    commit tree b12abd6c99d973979da9e5c18c0e0e228a21e4c4

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arrayDrivers) {
+    return (arrayDrivers.slice(0, 2));
+}
+
+const returnLastTwoDrivers = function(arrayDrivers) {
+    return (arrayDrivers.slice(-2))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (int) {
+    return function (fare=5) {
+        return fare * int;
+    }
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+
+}
+
+function selectDifferentDrivers(arrayDrivers, fn) {
+    return fn(arrayDrivers);
+}
